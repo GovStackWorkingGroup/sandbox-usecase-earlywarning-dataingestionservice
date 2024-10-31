c90a3d0a0c28d5a013743b377d0a93228c355e94
oleksii <oleksiidn@proton.me>
2024-10-31 16:34:24 +0200

    Names update to align with threat microservice Added missed application properties for kafka

--- src/main/java/global/govstack/data_ingestion/dto/ThreatDto.java
+++ src/main/java/global/govstack/data_ingestion/dto/ThreatDto.java
@@ -2,5 +2,5 @@ package global.govstack.data_ingestion.dto;
 
 import java.util.List;
 
-public record ThreatDto(String threatType, String severity, List<String> affectedCountries, List<String> affectedCounties, String range, String periodStart, String periodEnd) {
+public record ThreatDto(String type, String severity, List<String> affectedCountries, List<String> affectedCounties, String range, String periodStart, String periodEnd) {
 }
